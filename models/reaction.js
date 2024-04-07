d22388c86c2db03439dba89611769a5e5cf6feb5
Dilek Nuredin <dileknuredin@hotmail.com>
2024-04-07 00:02:29 -0400

    added routes

--- models/reaction.js
+++ models/reaction.js
@@ -1,33 +1,32 @@
 const { schema, types } = require("mongoose");
 
 const reactionSchema = new schema(
-
-    {
-        reactionId: {
-            type: schema.types.ObjectId,
-            default: () => new types.ObjectId(),
-        },
-        reactionBody: {
-            type: types.String,
-            required: true,
-            max: 280,
-        },
-        username: {
-            type: types.String,
-            required: true,
-        },
-        createdAt: {
-            type: types.Date,
-            default: Date.now,
-            get: (timestamp) => dateFormat(timestamp).toLocalDateString(),
-        },
+  {
+    reactionId: {
+      type: schema.types.ObjectId,
+      default: () => new types.ObjectId(),
     },
-    {
-        toJSON: {
-            getters: true,
-        },
-        id: false,
-    }
+    reactionBody: {
+      type: types.String,
+      required: true,
+      max: 280,
+    },
+    username: {
+      type: types.String,
+      required: true,
+    },
+    createdAt: {
+      type: types.Date,
+      default: Date.now,
+      get: (timestamp) => dateFormat(timestamp).toLocalDateString(),
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
 );
 
-module.exports = reactionSchema; 
+module.exports = reactionSchema;
